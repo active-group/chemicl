5dab1026173ffcc83849e7ca067b4f663bc2e329
Markus Schlegel <markus.schlegel@active-group.de>
2019-01-24 11:47:33 +0100

    Tests for `swap`, `cas`, and `read`

--- test/chemicl/reagents_test.clj
+++ test/chemicl/reagents_test.clj
@@ -8,6 +8,11 @@
             [chemicl.message-queue :as mq]
             [clojure.test :as t :refer [deftest testing is]]))
 
+
+;; ----------------------
+;; --- Update -----------
+;; ----------------------
+
 (deftest update-t
   (let [result-res (atom nil)
         ts (atom (refs/make-ref 1337 []))
@@ -63,49 +68,148 @@
     (is (= @starter-res :starter-res))))
 
 
+;; ----------------------
+;; --- Swap -------------
+;; ----------------------
+
+(deftest swap-t
+  (let [res-1 (atom nil)
+        res-2 (atom nil)
+
+        ;; FIXME: this makes too many assumptions about the
+        ;; internal structure of message queues and endpoints
+        mq1 (atom [])
+        mq2 (atom [])
+
+        ep-1 (channels/make-endpoint mq1 mq2)
+        ep-2 (channels/make-endpoint mq2 mq1)
+
+        swapper (fn [ep v res-atom]
+                  (m/monadic
+                   [res (rea/react! (rea/swap ep) v)]
+                   (let [_ (reset! res-atom res)])
+                   (conc/print "done" (pr-str res))))
+
+        swap-1 (swapper ep-1 :from-1 res-1)
+        swap-2 (swapper ep-2 :from-2 res-2)
+        ]
+
+    ;; Run swap-1
+    (conc/run-many-to-many swap-1)
+
+    ;; Let swap-1 dangle for a while
+    (Thread/sleep 100)
+
+    ;; Run swap-2
+    (conc/run-many-to-many swap-2)
+
+    ;; Wait
+    (Thread/sleep 20)
+
+    ;; Check results
+    (is (= @res-1 :from-2))
+    (is (= @res-2 :from-1))
+    ))
 
 
+(deftest swap-with-upd-continuation-t
+  (let [res-1 (atom nil)
+        res-2 (atom nil)
+
+        ref-1 (atom (refs/make-ref :nothing []))
+        ref-2 (atom (refs/make-ref :nothing []))
+
+        ;; FIXME: this makes too many assumptions about the
+        ;; internal structure of message queues and endpoints
+        mq1 (atom [])
+        mq2 (atom [])
+
+        ep-1 (channels/make-endpoint mq1 mq2)
+        ep-2 (channels/make-endpoint mq2 mq1)
+
+        swapper (fn [ep v res-atom r]
+                  (m/monadic
+                   [res (rea/react! (rea/>>>
+                                     (rea/swap ep)
+                                     (rea/upd r (fn [[ov a]]
+                                                  ;; store input at ref 
+                                                  [(+ a 100)
+                                                   (+ a 1000)]))) v)]
+                   (let [_ (reset! res-atom res)])
+                   (conc/print "done" (pr-str res))))
+
+        swap-1 (swapper ep-1 1 res-1 ref-1)
+        swap-2 (swapper ep-2 2 res-2 ref-2)
+        ]
+
+    ;; Run swap-1
+    (conc/run-many-to-many swap-1)
+
+    ;; Let swap-1 dangle for a while
+    (Thread/sleep 100)
+
+    ;; Run swap-2
+    (conc/run-many-to-many swap-2)
+
+    ;; Wait
+    (Thread/sleep 20)
+
+    ;; Check results
+    (is (= @res-1 1002)) ;; 2 (swap) + 1000 (upd)
+    (is (= @res-2 1001)) ;; 1 (swap) + 1000 (upd)
+
+    ;; Check refs
+    (is (= (refs/ref-data @ref-1) 102))
+    (is (= (refs/ref-data @ref-2) 101))
+    ))
 
 
+;; ----------------------
+;; --- CAS --------------
+;; ----------------------
+
+(deftest cas-t
+  (let [res (atom nil)
+        ref (atom (refs/make-ref :nothing []))
+        rea (rea/cas ref :nothing :something)]
+
+    ;; run cas reagent
+    (conc/run-many-to-many
+     (m/monadic
+      [out (rea/react! rea nil)]
+      (let [_ (reset! res out)])
+      (conc/print "done")))
+
+    ;; wait
+    (Thread/sleep 20)
+
+    ;; Check ref
+    (is (= (refs/ref-data @ref) :something))
+
+    ;; Check result
+    (is (= nil @res))
+    ))
 
 
+;; ----------------------
+;; --- Read -------------
+;; ----------------------
 
+(deftest read-t
+  (let [res (atom nil)
+        ref (atom (refs/make-ref :bounty []))
+        rea (rea/read ref)]
 
+    ;; run read reagent
+    (conc/run-many-to-many
+     (m/monadic
+      [out (rea/react! rea nil)]
+      (let [_ (reset! res out)])
+      (conc/print "done")))
 
-(def mq1 (atom []))
-(def mq2 (atom []))
+    ;; wait
+    (Thread/sleep 20)
 
-(def ep1 (channels/make-endpoint mq1 mq2))
-(def ep2 (channels/make-endpoint mq2 mq1))
-
-(pr-str ep1)
-
-#_(defmonadic swapper [ep v]
-  [res (rea/react! (rea/swap ep) v)]
-  (conc/print "got: " (pr-str res))
-  )
-
-(defmonadic swapper-1 []
-  (conc/print "--- swapper-1 ---")
-  [res (rea/react! (rea/swap ep1) :from-1)]
-  (conc/print "1 got" (pr-str res)))
-
-(defmonadic swapper-2 []
-  (conc/print "--- swapper-2 ---")
-  [res (rea/react! (rea/swap ep2) :from-2)]
-  (conc/print "2 got" (pr-str res)))
-
-(conc/run-many-to-many (swapper-1))
-(conc/run-many-to-many (swapper-2))
-
-
-#_(conc/run-many-to-many (m/monadic
-                        (conc/swapm
-                         r
-                         (fn [v]
-                           (m/monadic
-                            (conc/print "hi")
-                            (m/return (inc v))
-                            )))
-                        [now (conc/read r)]
-                        (conc/print "now: " (pr-str now))))
+    ;; Check result
+    (is (= :bounty @res))
+    ))
