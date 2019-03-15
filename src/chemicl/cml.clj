895ddc3f42c3a33a1b2069b312320b1428d694dd
David Frese <david.frese@active-group.de>
2019-03-15 15:01:51 +0100

    Add an implementation of 'concurrent ml' based on reagents.

---
+++ src/chemicl/cml.clj
@@ -0,0 +1,234 @@
+(ns chemicl.cml
+  (:require [chemicl.reagents :as r]
+            [chemicl.channels :as ch]
+            [chemicl.refs :as refs]
+            [chemicl.timeout :as tmo]
+            [chemicl.concurrency :as c]
+            [active.clojure.monad :as monad])
+  (:refer-clojure :exclude [send sync resolve]))
+
+(defprotocol Event
+  (resolve [this] "Is called at 'synchronization time', and must
+  return a tuple `[reagent abort]`, where abort is nil, or a function
+  of no arguments, that is called when this event is not selected in
+  a [[choose]], and must return an action in the concurrency
+  monad. That action is scheduled on a new thread after the
+  synchronization."))
+
+(defn event?
+  "Returns wheather v is an event."
+  [v]
+  (satisfies? Event v))
+
+(defn- comp-abort [& aborts]
+  (reduce (fn [res ab]
+            (if (some? ab)
+              (if (some? res)
+                (fn []
+                  (monad/sequ_ (list (res) (ab))))
+                ab)
+              res))
+          nil
+          aborts))
+
+(defn- demonad [m] ;; TODO: this should not be needed.
+  @(c/run-many-to-many m))
+
+;; the primitives
+
+(defrecord ^:private Never
+  []
+  Event (resolve [_]
+          [r/never nil]))
+
+(defrecord ^:private Always
+  [v]
+  Event (resolve [_]
+          [(r/return v) nil]))
+
+(defrecord ^:private Send
+  [ch v]
+  Event (resolve [_]
+          [(r/>>> (r/return v) (r/send ch)) nil]))
+
+(defrecord ^:private Receive
+  [ch]
+  Event (resolve [_]
+          [(r/receive ch) nil]))
+
+(defrecord ^:private Wrap
+  [ev f args]
+  Event (resolve [_]
+          (let [[r abort] (resolve ev)]
+            [(r/>>> r (r/lift (fn [v] (apply f v args))))
+             abort])))
+
+(defrecord ^:private WrapAbort
+  [ev f args]
+  Event (resolve [_]
+          (let [[reagent a] (resolve ev)
+                ab (fn [] ;; TODO fork should be this.
+                     (c/fork (apply f args)))]
+            [reagent (comp-abort a ab)])))
+
+(defrecord ^:private Guard
+  [f args]
+  Event (resolve [_]
+          (let [ev (apply f args)]
+            (resolve ev))))
+
+(defrecord ^:private Timeout
+  [ms]
+  Event (resolve [_]
+          [(tmo/timeout ms) nil]))
+
+;; the api
+
+(def ^{:doc "An event that will never become available."}
+  never
+  (Never.))
+
+(defn always
+  "Returns an event that is available immediately, yielding the given
+  value."
+  [v]
+  (Always. v))
+
+(defn channel
+  "Returns a new synchronous channel."
+  []
+  (demonad (ch/new-channel)))
+
+(defn channel?
+  "Return wheather v is a [[channel]]."
+  [v]
+  (ch/channel? v))
+
+(defn send
+  "Returns an event that will become available after the given value `v`
+  could be sent over the given channel `ch`."
+  [ch v]
+  (assert (channel? ch) (pr-str ch))
+  (Send. ch v))
+
+(defn receive
+  "Returns an event that will become available after a value could be
+  received over the given channel `ch`, yielding that value."
+  [ch]
+  (assert (channel? ch) (pr-str ch))
+  (Receive. ch))
+
+(defn wrap
+  "Returns an event equivalent to `ev`. After that is synchronized upon,
+  `f` will be invoked on the resulting value, and it's result will
+  become the value of the event."
+  [ev f & args]
+  (assert (event? ev) (pr-str ev))
+  (assert (ifn? f) (pr-str f))
+  (Wrap. ev f args))
+
+(defn wrap-abort-m [ev f & args]
+  (assert (event? ev) (pr-str ev))
+  (assert (ifn? f) (pr-str f))
+  (WrapAbort. ev f args))
+
+(defn- monad-lift [f & args]
+  (monad/free-bind (monad/return nil)
+                   (fn [_] (monad/return (apply f args)))))
+
+(defn wrap-abort [ev f & args]
+  (assert (ifn? f) (pr-str f))
+  (apply wrap-abort-m ev monad-lift f args))
+
+(defn guard
+  "Returns an event that when it's synchronized, `f` will be called which
+  must return an event which is then synchronized upon instead."
+  [f & args]
+  (assert (ifn? f) (pr-str f))
+  (Guard. f args))
+
+(defn timeout
+  "Returns an event that will become available the given amount of
+  milliseconds after synchronization."
+  [ms] ;; TODO: add time unit, or a nanosecond variant?
+  (Timeout. ms))
+
+;; With-nack could be defined in terms of with-abort and a channel, but it's a little more efficient with one more primitive: the ref even
+
+(defrecord RefIs [ref predicate]
+  Event (resolve [_]
+          [(r/upd ref
+                  (fn [[old input]]
+                    (when (predicate old)
+                      [old old])))
+           nil]))
+
+(defn ref-is
+  "Returns an event that becomes available when the given reference
+  cell is set to a value for which predicate returns truthy. The
+  current value of the ref is then the result of the event."
+  [ref predicate]
+  (RefIs. ref predicate))
+
+(defn- with-nack-internal [f args]
+  (let [ref (demonad (refs/new-ref false))
+        nack-ev (wrap (ref-is ref identity)
+                      (constantly nil))
+        nack (fn []
+               (refs/reset ref true))]
+    (wrap-abort-m (apply f nack-ev args)
+                  nack)))
+
+(defn with-nack
+  "Returns an event, for which `(f nack-ev & args)` is called at
+  synchronization time, which must return another event which is then
+  synchronized on instead. The given `nack-ev` becomes available if
+  this event is not the selected one in the synchronization of
+  a [[choose]] event."
+  [f & args]
+  (guard with-nack-internal f args))
+
+(defn- abort-others [aborts-map except-idx]
+  (monad/sequ_ (reduce-kv (fn [r ai abort]
+                            (if (or (= ai except-idx) (nil? abort))
+                              r
+                              (conj r (abort))))
+                          []
+                          aborts-map)))
+
+(defrecord Choose [evs]
+  Event (resolve [_]
+          (let [reas-aborts (map resolve evs)
+                aborts-map (into {} (map-indexed vector
+                                                 (map second reas-aborts)))]
+            [(apply r/choose (map-indexed (fn [idx rea]
+                                            (r/>>> rea (r/post-commit (fn cho-post-abort [_]
+                                                                        (abort-others aborts-map idx)))))
+                                          (map first reas-aborts)))
+             ;; abort commit => abort all:
+             (when-not (empty? aborts-map)
+               (fn cho-abort-all []
+                 (abort-others aborts-map -1)))])))
+
+
+(defn choose
+  "Returns an event with will synchronize on one of the given events non-deterministically."
+  [& evs]
+  (assert (every? event? evs) (vec (remove event? evs)))
+  (Choose. evs))
+
+;; synchronization
+
+(defn sync-m "Synchronize the given event as a command in the concurrency monad." [ev]
+  (r/react! (first (resolve ev)) nil))
+
+(defn sync-p "Synchronize the given event, returning a promise of the result." [ev]
+  (c/run-many-to-many (sync-m ev)))
+
+(defn sync
+  "Synchronize the given event, blocking the current thread until it succeeds, resp. returning `timeout-val` after a timeout."
+  ([ev]
+   (deref (sync-p ev)))
+  ([ev timeout-ms timeout-val]
+   ;; TODO: there should be a synchronous run-many-to-many (with timeout), so we can do optimizations there.
+   (deref (sync-p ev) timeout-ms timeout-val)))
---
+++ test/chemicl/cml_test.clj
@@ -0,0 +1,183 @@
+(ns chemicl.cml-test
+  (:require [chemicl.cml :as cml]
+            [clojure.test :refer [deftest testing is]]))
+
+(def debug false)
+
+(defn sync-eventually [ev & [ms]] ;; for all that require thread changes/a bit of computation.
+  ;; Note: no timeout makes more stable tests, but is bad for debugging in case of dead-locks.
+  (if debug
+    (cml/sync ev
+              ;; Note: 100ms is actually the time it needs sometimes - seems really slow?!
+              (or ms 100) ::timeout)
+    (cml/sync ev)))
+
+(defn sync-now [ev] ;; for all events that should be immedately available.
+  (sync-eventually ev 2))
+
+(defn not-sync [ev & [ms]]
+  (= (cml/sync ev (or ms 100) ::timeout)
+     ::timeout))
+
+(deftest send-receive-test
+  (let [c (cml/channel)
+        started (promise)]
+    (future
+      (deliver started true)
+      (cml/sync (cml/send c 42)))
+    @started
+    (is (= (sync-eventually (cml/receive c))
+           42))))
+
+(deftest never-test
+  (is (not-sync cml/never)))
+
+(deftest always-test
+  (is (= (sync-now (cml/always 42))
+         42)))
+
+(deftest timeout-test
+  (is (not-sync (cml/timeout 20) 1))
+  (is (nil? (cml/sync (cml/timeout 1) 10 ::timeout)))
+  ;; also, timeouts start when sync is called, not when created:
+  (let [ev (cml/timeout 10)]
+    (Thread/sleep 15)
+    (is (not-sync ev 1))))
+
+(deftest choose-test
+  (is (= (sync-now (cml/choose cml/never
+                               (cml/always 42)))
+         42)))
+
+(deftest guard-test
+  (let [called (atom false)
+        ev (cml/guard (fn []
+                        (reset! called true)
+                        (cml/always 42)))]
+    (is (not @called))
+    (is (= (sync-now ev)
+           42))
+    (is @called)))
+
+(deftest with-nack-noop-test
+  (let [nack-ev (atom nil)]
+    (is (= (sync-now (cml/with-nack (fn [v]
+                                      (reset! nack-ev v)
+                                      (cml/always 42))))
+           42))
+    (is (not-sync @nack-ev))))
+
+(deftest with-nack-test
+  ;; branch not taken:
+  (let [nack-ev (atom nil)
+        ev (cml/choose (cml/with-nack (fn [v]
+                                        (reset! nack-ev v)
+                                        cml/never))
+                       (cml/always 42))]
+    (is (nil? @nack-ev))
+    (is (= (sync-now ev)
+           42))
+    (is (nil? (sync-eventually @nack-ev))))
+  ;; branch taken:
+  (let [nack-ev (atom nil)
+        ev (cml/choose (cml/with-nack (fn [v]
+                                        (reset! nack-ev v)
+                                        (cml/always 42)))
+                       cml/never)]
+    (is (nil? @nack-ev))
+    (is (= (sync-now ev)
+           42))
+    (is (not-sync @nack-ev 50))))
+
+(deftest with-nack-repeat-test
+  ;; what happens when a with-nack is used again?  (TODO: what happens in the original CML?)
+  (let [nack-ev1 (atom nil)
+        nack-ev2 (atom nil)
+        
+        run (atom nack-ev1)
+        inner (atom cml/never)
+        ev (cml/choose (cml/with-nack (fn [v]
+                                        (reset! @run v)
+                                        @inner))
+                       (cml/always 42))]
+    (sync-now ev)
+    (is (nil? (sync-eventually @nack-ev1)))
+
+    ;; second time that branch is chosen, so the second nack does not sync:
+    (reset! run nack-ev2)
+    (reset! inner (cml/always 21)) ;; <- inner could also be a channel that's now available
+    (is (= (sync-now ev)
+           21))
+    (is (not-sync @nack-ev2 50))))
+
+#_(deftest with-nack-nested-test
+  ;; Note: not sure if this is valid cml, or makes sense ;-)
+  (let [nack-ev2 (atom nil)
+        ev (cml/choose (cml/with-nack (fn [v]
+                                        (cml/with-nack (fn [v]
+                                                         (reset! nack-ev2 v)
+                                                         cml/never))))
+                       (cml/always 42))]
+    (is (= (sync-now ev)
+           42))
+    (is (nil? (sync-eventually @nack-ev2)))))
+
+(deftest wrap-test
+  (let [ev (cml/wrap (cml/always 42)
+                     (fn [v]
+                       (/ v 2)))]
+    (is (= (sync-now ev)
+           21))))
+
+(deftest performance-test
+  (let [run-for-secs 20]
+    (println)
+    (let [start (delay (java.time.Instant/now))
+
+          result
+          (fn [start i]
+            (let [ms (.until start (java.time.Instant/now) java.time.temporal.ChronoUnit/MILLIS)
+                  msg_per_sec (if (zero? ms)
+                                "n/a"
+                                (long (/ (double i) (/ (double ms) 1000))))
+                  ms_per_msg (if (zero? i)
+                               "n/a"
+                               (double (/ ms i)))]
+              (str msg_per_sec " messages per second, resp. " ms_per_msg " ms per message. ")))
+          
+          print-throughput
+          (fn [i]
+            (try (print "\r" (result @start i))
+                 (catch Exception e
+                   (println "You silly:" e))))
+
+          stop-ch (cml/channel)
+          stop-ev (cml/receive stop-ch)
+
+          ch (cml/channel)
+          ;; writer thread
+          writer (future
+                   (loop [i 0]
+                     (when-not (cml/sync (cml/choose stop-ev
+                                                     (cml/send ch i)))
+                       (recur (inc i)))))
+          reader (future
+                   (loop [i nil]
+                     (let [in (cml/sync (cml/choose stop-ev
+                                                    (cml/receive ch)))]
+                       (if-not (= true in)
+                         (do
+                           (when-not (or (nil? i) (= (inc i) in))
+                             (println "***** INVARIANT VIOLATION: Missed message *****"))
+                           (if (zero? in) ;; first as a warmup
+                             @start
+                             (when (zero? (mod (dec in) 100))
+                               (print-throughput in)))                         
+                           (recur in))
+                         i))))]
+      (cml/sync (cml/timeout (* 1000 run-for-secs)))
+      (cml/sync (cml/send stop-ch true))
+      (cml/sync (cml/send stop-ch true))
+      @writer
+      (let [total @reader]
+        (println "\r" (result @start total))))))
